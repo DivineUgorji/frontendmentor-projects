3e0a483610132518b694feff95ddfbfef679f842
Divine Ugorji <divineugorji37@gmail.com>
2025-09-15 22:49:35 +0100

    age calculator: initial commit 🎉

---
+++ age-calculator-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-5xl">Hello Age Calculator App</h1>
+    </div>
+  );
+};
+
+export default App;
